4c1811e1adbc9a74c1cca7e980e8c76b357293f6
Kshitiz Gupta <gupta.ks@northeastern.edu>
2021-03-15 12:02:51 -0400

    moved exception class inside the benefits folder

---
+++ benefits/src/main/java/com/paylocity/benefits/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.paylocity.benefits.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectNotFoundException(EmployeeNotFoundException ex, WebRequest webRequest){
+
+        EmployeeNotFoundException exceptionResponse = new EmployeeNotFoundException(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
